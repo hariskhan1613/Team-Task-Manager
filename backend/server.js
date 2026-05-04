de9105f93d1cc424a72fc0ee76ac8c170f91f9fe
Mohd Haris Khan <hariskhan9456973386@gmail.com>
2026-05-04 14:30:23 +0530

    fix frontend url

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS CONFIG (FIXED FOR LOCAL + PRODUCTION)
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://team-task-manager-1wve.vercel.app/"
+  "https://team-task-manager-1wve.vercel.app"
 ];
 
 app.use(
